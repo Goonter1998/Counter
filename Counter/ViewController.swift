612477f9b3049993ba564c913c31c24f6ed330ca
Goonter1998 <9289641@gmail.com>
2023-05-15 13:38:05 +0300

    12 - Добавлен Аутлет с текстом 14 - Добавлено изначальное число счетчика = 0 21 - Добавлен Action кнопки 23-24 - Добавлено прибавление к счету +1 за нажатие и вывод нового значения

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func bbuttonTapped(_ sender: Any) {
+        
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
 }
 
